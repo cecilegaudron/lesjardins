04e328f0dd815b04110ac2851dd202fdbc6a3f83
cecilegaudron <ce.gaudron@gmail.com>
2023-05-04 14:59:07 +0000

    Add quantity boxes to shopping bag page

--- bag/contexts.py
+++ bag/contexts.py
@@ -16,18 +16,21 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
+    for item_id, item_data in bag.items():
         """
         Display the items on the shopping bag
         """
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
     """
     Checker if the amount to pay is less than the total threshold
     If yes, the delivery is calculated as the total multiplied by the standard delivery percentage so 10%
