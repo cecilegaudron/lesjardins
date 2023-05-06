32274809e874a350409e4e40ce57bc14ebca23a4
cecilegaudron <ce.gaudron@gmail.com>
2023-05-06 10:08:42 +0000

    Add toast messages for success warning error etc.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -11,6 +13,7 @@ def add_to_bag(request, item_id):
     """
     Basic view to add a quantity of the specified product to the shopping bag
     """
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -21,6 +24,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'You add {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
