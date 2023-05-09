763c1a5578ca931be6b1eb6cd36d0309890e65e1
cecilegaudron <ce.gaudron@gmail.com>
2023-05-09 08:41:31 +0000

    Delete useless white space

--- checkout/signals.py
+++ checkout/signals.py
@@ -14,4 +14,3 @@ def update_on_save(sender, instance, created, **kwargs):
 def update_on_save(sender, instance, created, **kwargs):
     """ Update order total on lineitem delete """
     instance.order.update_total()
-    
