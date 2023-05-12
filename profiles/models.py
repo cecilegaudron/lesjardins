daeb28c545332d76bbacd654969aba4cff70491c
cecilegaudron <ce.gaudron@gmail.com>
2023-05-12 13:11:12 +0000

    Update allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -44,6 +44,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """Create or update the user profile"""
