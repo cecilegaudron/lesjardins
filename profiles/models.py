b4b24dac1b532c9ba8e3495c887cbc4a6fd73446
cecilegaudron <ce.gaudron@gmail.com>
2023-05-12 13:49:36 +0000

    Update profile page

--- profiles/models.py
+++ profiles/models.py
@@ -9,14 +9,14 @@ class UserProfile(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE
-        )
-    default_phone_number = models.CharField(
-        max_length=20,
+        )    
+    default_street_address1 = models.CharField(
+        max_length=80,
         null=True,
         blank=True
         )
-    default_country = models.CharField(
-        max_length=40,
+    default_street_address2 = models.CharField(
+        max_length=80,
         null=True,
         blank=True
         )
@@ -30,13 +30,13 @@ class UserProfile(models.Model):
         null=True,
         blank=True
         )
-    default_street_address1 = models.CharField(
-        max_length=80,
+    default_country = models.CharField(
+        max_length=40,
         null=True,
         blank=True
         )
-    default_street_address2 = models.CharField(
-        max_length=80,
+    default_phone_number = models.CharField(
+        max_length=20,
         null=True,
         blank=True
         )
