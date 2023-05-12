59d3327adbc762b44f86b135d8442e72ea67787f
cecilegaudron <ce.gaudron@gmail.com>
2023-05-12 08:32:51 +0000

    Remove an useless part of code

--- checkout/forms.py
+++ checkout/forms.py
@@ -15,23 +15,6 @@ class OrderForm(forms.ModelForm):
             'postcode',
             'country',
         )
-from django import forms
-from .models import Order
-
-
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = (
-            'full_name',
-            'email',
-            'phone_number',
-            'street_address1',
-            'street_address2',
-            'city',
-            'postcode',
-            'country',
-        )
 
     def __init__(self, *args, **kwargs):
         """
