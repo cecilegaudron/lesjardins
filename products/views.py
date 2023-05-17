17d1fec0603e25db90f04ae5006e5be3d2cbad37
cecilegaudron <ce.gaudron@gmail.com>
2023-05-17 16:13:04 +0000

    Views for add, edit and delete products for admin

--- products/views.py
+++ products/views.py
@@ -118,9 +118,50 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """Add product to the store"""
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'You added a product with success')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Adding product failes. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """Edit a product in the store"""
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product updated!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Updating product failed. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """Delete a product from the store"""
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Products deleted!')
+    return redirect(reverse('products'))
