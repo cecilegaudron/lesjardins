a42e40fd87a69c8b1b6818fd11b86a8b740ddee0
cecilegaudron <ce.gaudron@gmail.com>
2023-05-23 09:50:38 +0000

    Turn off debug setting

--- lesjardins/settings.py
+++ lesjardins/settings.py
@@ -32,7 +32,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = False
+#DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = [
     '8000-cecilegaudro-lesjardins-qz5xotnyoyp.ws-eu97.gitpod.io',
