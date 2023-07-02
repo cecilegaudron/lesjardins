5dcc72c568bef654b843a898c286fef04493a98d
cecilegaudron <ce.gaudron@gmail.com>
2023-07-02 10:39:56 +0000

    Remove useless line

--- bag/views.py
+++ bag/views.py
@@ -9,6 +9,7 @@ def view_bag(request):
     """
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """
     Basic view to add a quantity of the specified product to the shopping bag
