d12f77809ce195858e910fe716d2157e48da67ef
cecilegaudron <ce.gaudron@gmail.com>
2023-07-05 19:03:24 +0000

    Correct spelling mistake

--- bag/views.py
+++ bag/views.py
@@ -26,7 +26,7 @@ def add_to_bag(request, item_id):
         messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
-        messages.success(request, f'You aded {product.name} to your bag')
+        messages.success(request, f'You added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
