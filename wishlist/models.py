7ac074895386a2f589cfe43c8ac7845499e42ab5
cecilegaudron <ce.gaudron@gmail.com>
2023-07-18 19:07:13 +0000

    Fix wishlist, display list and add Because wishlist was not linked with user

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from products.models import Product
-from django.contrib.auth.models import User
+from profiles.models import UserProfile
 
 
 class Wishlist(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE
+    user_favourite = models.ForeignKey(
+        UserProfile,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='favourites'
     )
     favourite = models.ForeignKey(
         Product,
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE
     )
     slug = models.CharField(
         max_length=30,
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,56 +1,48 @@
 from django.shortcuts import render, reverse, get_object_or_404, HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Wishlist, Product
+from profiles.models import UserProfile
 
-
+ 
 # https://data-flair.training/blogs/python-django-wishlist-app
+@login_required
 def favourite_list(request):
     """
     Basic view to return the wishlist page
     """
-    user = request.user
-    favourite = Wishlist.objects.all()
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    favourites = profile.favourites.all()
+
+    template = 'wishlist/wishlist.html'
     context = {
-        'favourites': favourite
+        'favourites': favourites,
+        'on_profile_page': True
     }
 
-    return render(request, 'wishlist/wishlist.html', context)
-
-
-def favourite_product(request, item_id):
-    """ A view to show individual product details """
-
-    item = get_object_or_404(Product, pk=item_id)
-    is_favourite = False
-
-    if product.favourites.filter(id=request.user.id).exists():
-        is_favourite = True
-
-    context = {
-        'product': product,
-        'is_favourite': is_favourite,
-    }
-
-    return render(request, 'products/product_detail.html', context)
+    return render(request, template, context)
 
 
 # https://stackoverflow.com/questions/56580696/how-to-implement-add-to-wishlist-for-a-product-in-django
+@login_required
 def add_to_wishlist(request, item_id):
     """
     Basic view to add a product to the wishlist
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
     item = get_object_or_404(Product, pk=item_id)
 
-    favourite, created = Wishlist.objects.get_or_create(
+    favourites, created = profile.favourites.get_or_create(
         favourite=item,
         favourite_id=item.id,
-        user=request.user,
     )
     messages.success(request, 'The product was added to your wishlist')
     return HttpResponseRedirect(reverse('product_detail', args=[str(item_id)]))
 
 
 # https://stackoverflow.com/questions/63444550/how-to-delete-product-from-wishlist-in-django
+@login_required
 def remove_to_wishlist(request, item_id):
     """
     Basic view to remove a product from the wishlist
