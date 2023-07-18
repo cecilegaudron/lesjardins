199ffcf62940eee0b235fd28c4e313f0d39e5c47
cecilegaudron <ce.gaudron@gmail.com>
2023-07-18 20:39:06 +0000

    Change display list for wishlist on admin panel

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Wishlist
 
 
-admin.site.register(Wishlist)
+# https://realpython.com/customize-django-admin-python/
+#https://stackoverflow.com/questions/32443471/django-show-the-count-of-related-objects-in-admin-list-display
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    """
+    View to display wishlist in Admin Panel
+    """
+    list_display = ("favourite", "user_favourite", "view_wishproduct")
+    
+    def view_wishproduct(self, obj):
+        return obj.view_wishproduct
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(view_wishproduct=Count("user_favourite"))
+        return queryset
